fd915b27657a57bf0cbd32771a872dc563da12b4
Oseias Isisdoro <oseiasisidoro@gmail.com>
2022-09-01 16:10:26 -0300

    create and implement LeagueController.php

---
+++ app/Http/Controllers/LeagueController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\League;
+use Illuminate\Http\Request;
+
+class LeagueController extends Controller
+{
+    public function index()
+    {
+        $leagues = League::orderBy('id', 'DESC')->paginate(10);
+
+        return view('home', compact('leagues'));
+    }
+
+    public function show(League $league) {
+        return view('leagues.show', compact('league'));
+    }
+}
