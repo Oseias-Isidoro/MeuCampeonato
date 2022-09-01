9cda6d1e7d8f6c77b8ddefa4d96a15cffae90c4e
Oseias Isisdoro <oseiasisidoro@gmail.com>
2022-09-01 10:18:54 -0300

    create model league

---
+++ app/Models/League.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
+
+/**
+ * @method static create(mixed $validated)
+ * @property mixed $id
+ * @property string $status
+ */
+class League extends Model
+{
+    use HasFactory, softDeletes;
+
+    public $timestamps = true;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'status'
+    ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function finish()
+    {
+        $this->status = 'finished';
+        $this->save();
+    }
+}
